810e7ba6a704405177ab23f255269f7d55a35fe3
siyangsun <siyangsun2007@gmail.com>
2017-03-12 23:06:13 -0700

    put everything into a new class, "Game"

--- game.cpp
+++ game.cpp
@@ -1,7 +1,41 @@
 #include "game.h"
-
+#include <QApplication>
+#include <QGraphicsScene>
+#include "player.h"
+#include "enemy.h"
+#include "melee_enemy.h"
+#include <QGraphicsView>
+#include <QTimer>
 
 Game::Game(QWidget *parent)
 {
+    Q_UNUSED(parent);
 
+
+    // create a scene
+    QGraphicsScene * scene = new QGraphicsScene();
+    // add a view
+    QGraphicsView *view = new QGraphicsView(scene);
+    // can also use view->setScene(scene);
+    view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    view->show();
+    view->setFixedSize(800 ,600);
+    scene->setSceneRect(0, 0, 800, 600);
+
+    //create the player
+    Player *player = new Player();
+    player->setRect(0, 0, 50, 50);
+    scene->addItem(player);
+    player->setFlag(QGraphicsItem::ItemIsFocusable);
+    player->setFocus();
+    player->setPos(view->width() / 2 - (player->rect().width() / 2), view->height() - player->rect().height());
+
+
+    //start spawning enemies
+    QTimer *timer = new QTimer();
+    QObject::connect(timer, SIGNAL(timeout()), player, SLOT(spawn_melee()));
+    timer->start(2000);
+
+    show();
 }
--- main.cpp
+++ main.cpp
@@ -1,46 +1,14 @@
 #include <QApplication>
-#include <QGraphicsScene>
-#include "player.h"
-#include "enemy.h"
-#include "melee_enemy.h"
-#include <QGraphicsView>
-#include <QTimer>
+#include "game.h"
+
+Game *game;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    // create a scene
-    QGraphicsScene * scene = new QGraphicsScene();
-
-    // create an item put into the scene that can respond to key events
-    Player *player = new Player();
-    player->setRect(0, 0, 50, 50);
-
-    // add the item to the scene
-    scene->addItem(player);
-
-    // make the rect focusable
-    player->setFlag(QGraphicsItem::ItemIsFocusable);
-    player->setFocus();
-
-    // add a view
-    QGraphicsView *view = new QGraphicsView(scene);
-    // can also use view->setScene(scene);
-
-    view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-
-    view->show();
-    view->setFixedSize(800 ,600);
-    scene->setSceneRect(0, 0, 800, 600);
-
-    player->setPos(view->width() / 2 - (player->rect().width() / 2), view->height() - player->rect().height());
-
-    //start spawning enemies
-    QTimer *timer = new QTimer();
-    QObject::connect(timer, SIGNAL(timeout()), player, SLOT(spawn_melee()));
-    timer->start(2000);
+    game = new Game();
+    game->show();
 
     return a.exec();
 }
